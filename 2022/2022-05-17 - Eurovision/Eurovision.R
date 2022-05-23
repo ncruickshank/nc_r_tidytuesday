3dbae7217954301ae225fa26b13ae14d7c893e8c
Nick Cruickshank <64281712+ncruickshank@users.noreply.github.com>
2022-05-22 20:21:35 -0400

    finalize tidy tuesday plot

--- 2022/2022-05-17 - Eurovision/Eurovision.R
+++ 2022/2022-05-17 - Eurovision/Eurovision.R
@@ -16,17 +16,7 @@ f2 <- "IBM Plex Sans"
 font_add_google(f2)
 showtext_auto()
 
-
-# visualization
-## IDEA ONE
-### how did the release of Will Ferrell's movie impact Iceland's performance at
-### at Eurovision?
-### Likely most seen in televoting audience
-## IDEA TWO
-### which countries vote the most for iceland?
-
-
-# who votes for iceland?
+## who votes for iceland?
 vis <- votes %>%
   filter(
     year >= 2000,
@@ -49,7 +39,7 @@ is_top_voters <- vis %>%
   head(5) %>%
   pull(from_country)
 
-## update vis
+## update original df
 vis2 <- vis %>%
   mutate(
     from_country2 = ifelse(from_country %in% is_top_voters, from_country, "Other"),
@@ -78,18 +68,18 @@ is_top_performances <- eurovision %>%
   pull(label)
 
 ## plot
-vis2 %>%
+eurovision_iceland_plot <- vis2 %>%
   ggplot(aes(year, points)) + 
   # annotations
   ## title
   annotate(
-    "text", x = 2000, y = 475, label = "Iceland at the Eurovision Song Contest",
+    "text", x = 2000, y = 460, label = "Iceland at the Eurovision Song Contest",
     hjust = 0, family = f1, size = 12, fontface = "bold", color = "Yellow"
   ) + 
   ## subtitle
   annotate(
-    "text", x = 2000, y = 410, label = "Legend shows top counties (orderred top to bottom)\nwho vote for Iceland",
-    hjust = 0, family = f2, size = 6, lineheight = 0.8, color = "grey70"
+    "text", x = 2000, y = 410, label = "Legend shows top counties (ordered top to bottom) who vote for Iceland",
+    hjust = 0, family = f2, size = 5, lineheight = 0.8, color = "grey70"
   ) +
   ## top performances
   ### 2009
@@ -109,18 +99,54 @@ vis2 %>%
     "text", x = 2004.5, y = 325, label = is_top_performances[[4]],
     hjust = 0, family = f2, size = 2, lineheight = 0.8, color = "grey70"
   ) +
+  ### 2021
+  annotate(
+    "segment", x = 2019.5, xend = 2019.5, y = 175, yend = 325,
+    color = "grey70"
+  ) +
+  annotate(
+    "segment", x = 2019.5, xend = 2020.25, y = 325, yend = 335,
+    color = "grey70"
+  ) +
+  annotate(
+    "text", x = 2016, y = 200, label = is_top_performances[[1]],
+    hjust = 0, family = f2, size = 2, lineheight = 0.8, color = "grey70"
+  ) +
+  annotate(
+    "text", x = 2016, y = 275, label = is_top_performances[[2]],
+    hjust = 0, family = f2, size = 2, lineheight = 0.8, color = "grey70"
+  ) +
+  # will ferrell movie
+  annotate(
+    "segment", x = 2020, xend = 2020, y = 0, yend = 360,
+    linetype = "dashed", color = "yellow"
+  ) +
+  annotate(
+    "segment", x = 2019, xend = 2020, y = 370, yend = 360,
+    linetype = "dashed", color = "yellow"
+  ) +
+  annotate(
+    "text", x = 2018.5, y = 375, label = "Eurovision: Fire Saga",
+    color = "yellow", family = f1, size = 5, hjust = 1
+  ) +
+  annotate(
+    "text", x = 2018.5, y = 360, label = "Released on Netflix in 2020",
+    color = "grey70", family = f1, size = 3, hjust = 1
+  ) +
+  
+  labs(caption = "Data Source: Eurovision || Visualization: N. Cruickshank || #TidyTuesday") +
   
   # actual points accrued
   geom_bar(aes(fill = from_country2), color = "grey50", stat = "identity") + 
   scale_x_continuous(name = "Year", expand = c(0,0)) + 
   scale_y_continuous(name = "Total Points from Jury") +
   scale_fill_manual(values = c(
-    "The Netherlands" = "red",
-    "Malta" = "blue",
-    "Andorra" = "purple",
-    "Finland" = "pink",
-    "Serbia" = "yellow",
-    "Other" = "grey80"
+    "The Netherlands" = "navyblue",
+    "Malta" = "indianred1",
+    "Andorra" = "yellow",
+    "Finland" = "lightskyblue",
+    "Serbia" = "darkgoldenrod",
+    "Other" = "grey70"
   ), name = NULL) + 
   theme_minimal(base_family = f2) + 
   theme(
@@ -129,7 +155,13 @@ vis2 %>%
     panel.grid.major.y = element_line(color = "grey30"),
     panel.background = element_rect(fill = "grey15", color = "grey15"),
     plot.background = element_rect(fill = "grey15", color = "grey15"),
-    legend.text = element_text(size = 12, color = "grey50"),
+    legend.text = element_text(size = 12, color = "grey70"),
     axis.text = element_text(color = "grey50"),
-    axis.title = element_text(color = "grey50")
+    axis.title = element_text(color = "grey50"),
+    plot.caption = element_text(family = f2, color = "grey70", size = 10, hjust = 0.1)
   )
+
+eurovision_iceland_plot
+
+# dim 1000 x 550
+#ggsave("Iceland at Eurovision.png", plot = eurovision_iceland_plot, width = 10, height = 5.5, dpi = 500)
